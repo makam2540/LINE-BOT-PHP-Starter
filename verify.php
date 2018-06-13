67f6e97f9c351dff5e53e6c32750f91ff9b11b9b
TechnocomTnc <predapon.t@gmail.com>
2018-06-13 11:19:47 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,10 +1,9 @@
 <?php
-$access_token = 'EzgAlxIOWuvQE7ARrLvkdJclnDnkifxd0zVOgZ8DTvUm8HxrUcLcWhk9luK5+mra+zFZ7FrYjjbrFvQw84+Axi+P1zWPnxSCTl/lF5gVTDYBjp+XEJ8EjeyUYVhuvRlTscnsKgQN+zlfy+lk8jL9ywdB04t89/1O/w1cDnyilFU=
-';
+$access_token = 'EzgAlxIOWuvQE7ARrLvkdJclnDnkifxd0zVOgZ8DTvUm8HxrUcLcWhk9luK5+mra+zFZ7FrYjjbrFvQw84+Axi+P1zWPnxSCTl/lF5gVTDYBjp+XEJ8EjeyUYVhuvRlTscnsKgQN+zlfy+lk8jL9ywdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v2/oauth/verify';
 
-$headers = array('Authorization: Bearer ' . $access_token);
+$headers = array('Authorization: Bearer ' . 'EzgAlxIOWuvQE7ARrLvkdJclnDnkifxd0zVOgZ8DTvUm8HxrUcLcWhk9luK5+mra+zFZ7FrYjjbrFvQw84+Axi+P1zWPnxSCTl/lF5gVTDYBjp+XEJ8EjeyUYVhuvRlTscnsKgQN+zlfy+lk8jL9ywdB04t89/1O/w1cDnyilFU=');
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
