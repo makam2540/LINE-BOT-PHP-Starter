bda784a19dbe914b079cead0d5097c7d42eb8515
TechnocomTnc <predapon.t@gmail.com>
2018-06-13 10:15:38 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,2 +1,10 @@
 <?php
+
+curl -X POST \
+-H 'Content-Type:application/json' \
+-H 'Authorization: Bearer QV4Z3GwTNAwu+MVcwGCGAALxv9W/lHMLm6J/tvZvdS8eBf6A2nRlC1QdFvLI28iZ+zFZ7FrYjjbrFvQw84+Axi+P1zWPnxSCTl/lF5gVTDYFM8sdYtwxA1PVP5Tir5WzkwwsuzPxbs9IkWqxEvvcvQdB04t89/1O/w1cDnyilFU=
+' \
+...
+
+
 echo "I am a bot";
