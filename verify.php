801db9c90f91bc0195367e5b453ea46d6c2291d9
TechnocomTnc <predapon.t@gmail.com>
2018-06-13 11:21:06 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -3,7 +3,7 @@ $access_token = 'EzgAlxIOWuvQE7ARrLvkdJclnDnkifxd0zVOgZ8DTvUm8HxrUcLcWhk9luK5+mr
 
 $url = 'https://api.line.me/v2/oauth/verify';
 
-$headers = array('Authorization: Bearer ' . 'EzgAlxIOWuvQE7ARrLvkdJclnDnkifxd0zVOgZ8DTvUm8HxrUcLcWhk9luK5+mra+zFZ7FrYjjbrFvQw84+Axi+P1zWPnxSCTl/lF5gVTDYBjp+XEJ8EjeyUYVhuvRlTscnsKgQN+zlfy+lk8jL9ywdB04t89/1O/w1cDnyilFU=');
+$headers = array('Authorization: Bearer ' . $access_token);
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
