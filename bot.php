58228bc49b970b3693f535ebe30e885ba850e56f
TechnocomTnc <predapon.t@gmail.com>
2018-06-14 09:34:57 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,3 +1,76 @@
 <?php
 
-echo "I am a bot";
+    
+
+    $accessToken = "7YR60AJ855Zu1Etxsc7aCdFqhip1o8yAKj7PzLe90ClE9Po0fz5o81BeghtpCki4+zFZ7FrYjjbrFvQw84+Axi+P1zWPnxSCTl/lF5gVTDaDqdC5IHk30qnjo7GQ1hHKizexgGNpBPn/Fwz3slJqkQdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
+    
+    $content = file_get_contents('php://input');
+    $arrayJson = json_decode($content, true);
+    
+    $arrayHeader = array();
+    $arrayHeader[] = "Content-Type: application/json";
+    $arrayHeader[] = "Authorization: Bearer {$accessToken}";
+
+    //รับข้อความจากผู้ใช้
+    //$message = "ทำไร";
+    $message = $arrayJson['events'][0]['message']['text'];
+
+    //include('connect.php');
+   
+    //$sql = mysqli_connect("localhost","root","","db_junebot");
+    //$sql->set_charset("utf8");
+    
+   // $query = mysqli_query($sql,"select*from question ");
+  //  $row = mysqli_num_rows($query);
+    
+   // echo "==".$row;
+    
+    
+    // $data = mysqli_fetch_assoc($query);
+     
+     //echo $data['q_id'];
+     
+ /*    $query2 = mysqli_query($sql,"select*from answer where a_id = '$data['a_id']' ");
+    $data2 = mysqli_fetch_assoc($query2);
+
+    
+    $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+    $arrayPostData['messages'][0]['type'] = "text";
+    $arrayPostData['messages'][0]['text'] = "ffffasdasdasd"; //$data2['a_id']
+    replyMsg($arrayHeader,$arrayPostData);
+  */  
+
+    #ตัวอย่าง Message Type "Text"
+    if($message == "สวัสดี"){
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "text";
+        $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
+        replyMsg($arrayHeader,$arrayPostData);
+    }else {
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "text";
+        $arrayPostData['messages'][0]['text'] = "โทดที ยังโง่อยู่ เลยไม่เข้าใจ";
+        replyMsg($arrayHeader,$arrayPostData);        
+    }
+
+
+    
+function replyMsg($arrayHeader,$arrayPostData){
+        $strUrl = "https://api.line.me/v2/bot/message/reply";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$strUrl);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
+        curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($ch);
+        curl_close ($ch);
+    }
+
+
+   exit;
+
+
+?>
